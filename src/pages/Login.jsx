4d4e1e28ab2f40ad4f12961edce6f8eda19a078e
zm-chan <zmc951230@gmail.com>
2025-06-18 01:33:26 +0800

    Make login error message be specific

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,9 @@ import { useState } from "react";
 import { auth } from "../config/firebase";
 
 function Login() {
-  const [error, setError] = useState(false);
+  const [error, setError] = useState("");
+
+  console.log(error);
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -13,17 +15,18 @@ function Login() {
 
     try {
       await signInWithEmailAndPassword(auth, email, password);
-      setError(false);
+      setError("");
     } catch (error) {
-      setError(true);
+      // console.log(error.message);
+      setError(error.message);
     }
   }
 
   return (
-    <main className="bg-stone-800 min-h-dvh py-8 px-4 text-yellow-400 flex items-center justify-center">
+    <main className="flex min-h-dvh items-center justify-center bg-stone-800 px-4 py-8 text-yellow-400">
       <form
         onSubmit={handleSubmit}
-        className="grid w-5/6 max-w-xs gap-y-5 rounded-2xl px-4 py-6 text-center shadow-xl sm:max-w-sm sm:gap-y-7 sm:px-6 sm:py-8 sm:shadow-2xl lg:max-w-md lg:px-8 lg:py-10 bg-stone-900/70"
+        className="grid w-5/6 max-w-xs gap-y-5 rounded-2xl bg-stone-900/70 px-4 py-6 text-center shadow-xl sm:max-w-sm sm:gap-y-7 sm:px-6 sm:py-8 sm:shadow-2xl lg:max-w-md lg:px-8 lg:py-10"
       >
         <h2 className="text-xl font-semibold sm:text-2xl lg:text-3xl">
           Login Form
@@ -32,23 +35,28 @@ function Login() {
           name="email"
           type="email"
           placeholder="Email"
-          className="h-9 text-sm sm:py-5 sm:text-base lg:h-12 lg:text-lg border border-yellow-400 rounded-md px-3 py-2"
+          className="h-9 rounded-md border border-yellow-400 px-3 py-2 text-sm sm:py-5 sm:text-base lg:h-12 lg:text-lg"
         />
         <input
           name="password"
           type="password"
           placeholder="Password"
-          className="h-9 text-sm sm:py-5 sm:text-base lg:h-12 lg:text-lg border border-yellow-400 rounded-md px-3 py-2"
+          className="h-9 rounded-md border border-yellow-400 px-3 py-2 text-sm sm:py-5 sm:text-base lg:h-12 lg:text-lg"
         />
         <button
           type="submit"
-          className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium capitalize sm:py-5 sm:text-base lg:py-6 lg:text-lg h-10 px-4 py-2 bg-yellow-400 text-stone-800"
+          className="inline-flex h-10 items-center justify-center rounded-md bg-yellow-400 px-4 py-2 text-sm font-medium whitespace-nowrap text-stone-800 capitalize sm:py-5 sm:text-base lg:py-6 lg:text-lg"
         >
           Login
         </button>
-        {error && (
+        {error.includes("invalid") && (
           <span className="text-sm text-red-600 lg:text-lg">
             Wrong email or password!
+          </span>
+        )}
+        {error.includes("network") && (
+          <span className="text-sm text-red-600 lg:text-lg">
+            You are having network problem!
           </span>
         )}
       </form>
